3591ec2f95daf084c0dd6eeda6e995769eb0b423
jatin <adriano23012001@gmail.com>
2023-07-21 13:36:53 +0530

    basic publisher routes completed

--- src/routes/publisher.route.ts
+++ src/routes/publisher.route.ts
@@ -1,9 +1,21 @@
 import { Router } from "express";
-import { validateSignUp } from "../validators/publisher.validators";
-import { signup } from "../controllers/publisher.controller";
+import { validateActivationInfo, validateForgetPassInfo, validateResendTokenInfo, validateResetPassInfo, validateSignUp, validateSigninInfo, validateUpdatePublisherInfo, validateVerifyPassInfo } from "../validators/publisher.validators";
+import { activateAccount, deletePublisher, forgetPass, getUserData, resendToken, resetPass, signin, signout, signup, updatePublisher, verifyPass } from "../controllers/publisher.controller";
+import { verifyToken } from "../middleware/publisher.middleware";
 
 const router = Router();
 
 router.post("/signup", validateSignUp, signup);
+router.post("/resend-token", validateResendTokenInfo, resendToken);
+router.get("/verify",validateActivationInfo, activateAccount);
+router.post("/signin", validateSigninInfo, signin);
+router.get("/private-info", verifyToken, getUserData);
+router.post("/reset-pass", [verifyToken, validateResetPassInfo], resetPass);
+router.get("/signout", verifyToken, signout);
+router.post("/forgot-pass", validateForgetPassInfo, forgetPass);
+router.post("/verify-forgot", validateVerifyPassInfo, verifyPass);
+router.put("/update", [verifyToken, validateUpdatePublisherInfo], updatePublisher);
+router.delete("/delete", verifyToken, deletePublisher);
+
 
 export default router;
